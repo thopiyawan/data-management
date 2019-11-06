354a2b00fe18d0d50285e7d54f31f9b9adf4163f
piyawan <tho.piyawan@gmail.com>
2019-11-06 16:56:23 +0700

    Update GetMessageController.php

--- app/Http/Controllers/GetMessageController.php
+++ app/Http/Controllers/GetMessageController.php
@@ -117,16 +117,17 @@ class GetMessageController extends Controller
         $conn_string = "host=ec2-50-19-127-115.compute-1.amazonaws.com port=5432 dbname=d7g7emtks53g61 user=unzugplrlxhlus password=6c4119aeed2e68f47cb7f66d964e9d984471a6fc2bdabadba149f298eb40aa6b";
         $dbconn = pg_pconnect($conn_string);
 
-        $result = pg_query($dbconn,"SELECT seqcode FROM sequentsteps WHERE sender_id = '5555'");
+        // $result = pg_query($dbconn,"SELECT seqcode FROM sequentsteps WHERE sender_id = '5555'");
+        $insert_sequentsteps = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextseqcode,status)VALUES('{$user}','{$seqcode}','','{$nextseqcode}','1'") or die(pg_errormessage());
         $num = pg_num_rows($result);
-            if($num==0)         
-         {  
-             $seqcode = '0000';
-             $nextseqcode = '0000';             
-             $this->insert_sequentsteps($user,$seqcode,$nextseqcode);
-         }
+        //     if($num==0)         
+        //  {  
+        //      $seqcode = '0000';
+        //      $nextseqcode = '0000';             
+        //      $this->insert_sequentsteps($user,$seqcode,$nextseqcode);
+        //  }
 
-         dd($num);
+         dd($insert_sequentsteps);
                   
     }
      public function getmessage()
@@ -238,7 +239,7 @@ class GetMessageController extends Controller
     {          
         $conn_string = "host=ec2-50-19-127-115.compute-1.amazonaws.com port=5432 dbname=d7g7emtks53g61 user=unzugplrlxhlus password=6c4119aeed2e68f47cb7f66d964e9d984471a6fc2bdabadba149f298eb40aa6b";
         $dbconn = pg_pconnect($conn_string);
-        $insert_sequentsteps = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextseqcode,status)VALUES('{$user}','{$seqcode}','','{$nextseqcode}','1',NOW(),NOW())") or die(pg_errormessage());
+        $insert_sequentsteps = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextseqcode,status)VALUES('{$user}','{$seqcode}','','{$nextseqcode}','1'") or die(pg_errormessage());
         return $insert_sequentsteps;
     }
     public function seqcode_select($user)
