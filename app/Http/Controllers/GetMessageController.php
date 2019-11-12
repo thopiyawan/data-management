794131914f5c8cba4ab5ad00789408b9dac791d3
piyawan <tho.piyawan@gmail.com>
2019-11-12 11:05:42 +0700

    Update GetMessageController.php

--- app/Http/Controllers/GetMessageController.php
+++ app/Http/Controllers/GetMessageController.php
@@ -491,44 +491,44 @@ class GetMessageController extends Controller
                     break;
                 case 3 : 
                      // กำหนด action 4 ปุ่ม 4 ประเภท
-                     $actionBuilder = array(
-                        new MessageTemplateActionBuilder(
-                            'Message Template',// ข้อความแสดงในปุ่ม
-                            'This is Text' // ข้อความที่จะแสดงฝั่งผู้ใช้ เมื่อคลิกเลือก
-                        ),
-                        new UriTemplateActionBuilder(
-                            'Uri Template', // ข้อความแสดงในปุ่ม
-                            'https://www.ninenik.com'
-                        ),
-                        new DatetimePickerTemplateActionBuilder(
-                            'Datetime Picker', // ข้อความแสดงในปุ่ม
-                            http_build_query(array(
-                                'action'=>'reservation',
-                                'person'=>5
-                            )), // ข้อมูลที่จะส่งไปใน webhook ผ่าน postback event
-                            'datetime', // date | time | datetime รูปแบบข้อมูลที่จะส่ง ในที่นี้ใช้ datatime
-                            substr_replace(date("Y-m-d"),'T',10,1), // วันที่ เวลา ค่าเริ่มต้นที่ถูกเลือก
-                            substr_replace(date("Y-m-d",strtotime("+5 day")),'T',10,1), //วันที่ เวลา มากสุดที่เลือกได้
-                            substr_replace(date("Y-m-d"),'T',10,1) //วันที่ เวลา น้อยสุดที่เลือกได้
-                        ),      
-                        new PostbackTemplateActionBuilder(
-                            'Postback', // ข้อความแสดงในปุ่ม
-                            http_build_query(array(
-                                'action'=>'buy',
-                                'item'=>100
-                            )) // ข้อมูลที่จะส่งไปใน webhook ผ่าน postback event
-//                          'Postback Text'  // ข้อความที่จะแสดงฝั่งผู้ใช้ เมื่อคลิกเลือก
-                        ),      
-                    );
-                    $imageUrl = 'https://www.mywebsite.com/imgsrc/photos/w/simpleflower';
-                    $textMessageBuilder  = new TemplateMessageBuilder('Button Template',
-                        new ButtonTemplateBuilder(
-                                'button template builder', // กำหนดหัวเรื่อง
-                                NULL, // กำหนดรายละเอียด
-                                $imageUrl, // กำหนด url รุปภาพ
-                                $actionBuilder  // กำหนด action object
-                        )
-                    );              
+                                $actionBuilder = array(
+                                    new MessageTemplateActionBuilder(
+                                        'Message Template',// ข้อความแสดงในปุ่ม
+                                        'This is Text' // ข้อความที่จะแสดงฝั่งผู้ใช้ เมื่อคลิกเลือก
+                                    ),
+                                    new UriTemplateActionBuilder(
+                                        'Uri Template', // ข้อความแสดงในปุ่ม
+                                        'https://www.ninenik.com'
+                                    ),
+                                    new DatetimePickerTemplateActionBuilder(
+                                        'Datetime Picker', // ข้อความแสดงในปุ่ม
+                                        http_build_query(array(
+                                            'action'=>'reservation',
+                                            'person'=>5
+                                        )), // ข้อมูลที่จะส่งไปใน webhook ผ่าน postback event
+                                        'datetime', // date | time | datetime รูปแบบข้อมูลที่จะส่ง ในที่นี้ใช้ datatime
+                                        substr_replace(date("Y-m-d H:i"),'T',10,1), // วันที่ เวลา ค่าเริ่มต้นที่ถูกเลือก
+                                        substr_replace(date("Y-m-d H:i",strtotime("+5 day")),'T',10,1), //วันที่ เวลา มากสุดที่เลือกได้
+                                        substr_replace(date("Y-m-d H:i"),'T',10,1) //วันที่ เวลา น้อยสุดที่เลือกได้
+                                    ),      
+                                    new PostbackTemplateActionBuilder(
+                                        'Postback', // ข้อความแสดงในปุ่ม
+                                        http_build_query(array(
+                                            'action'=>'buy',
+                                            'item'=>100
+                                        )), // ข้อมูลที่จะส่งไปใน webhook ผ่าน postback event
+                                        'Postback Text'  // ข้อความที่จะแสดงฝั่งผู้ใช้ เมื่อคลิกเลือก
+                                    ),      
+                                );
+                                $imageUrl = 'https://www.mywebsite.com/imgsrc/photos/w/simpleflower';
+                                $textMessageBuilder = new TemplateMessageBuilder('Button Template',
+                                    new ButtonTemplateBuilder(
+                                            'button template builder', // กำหนดหัวเรื่อง
+                                            'Please select', // กำหนดรายละเอียด
+                                            $imageUrl, // กำหนด url รุปภาพ
+                                            $actionBuilder  // กำหนด action object
+                                    )
+                                );                   
                     break;
         
             }
