d3dc205ac7cd1caee7267792c851192048939425
piyawan <tho.piyawan@gmail.com>
2019-11-12 11:25:50 +0700

    Update GetMessageController.php

--- app/Http/Controllers/GetMessageController.php
+++ app/Http/Controllers/GetMessageController.php
@@ -193,37 +193,51 @@ class GetMessageController extends Controller
                 $seqcode = $this->seqcode_select($user);
 
 ///////////////////////////////////////////////////
+$replyToken = $events['events'][0]['replyToken'];
+$userID = $events['events'][0]['source']['userId'];
+$sourceType = $events['events'][0]['source']['type'];        
+$is_postback = NULL;
+$is_message = NULL;
+if(isset($events['events'][0]) && array_key_exists('message',$events['events'][0])){
+    $is_message = true;
+    $typeMessage = $events['events'][0]['message']['type'];
+    $userMessage = $events['events'][0]['message']['text'];     
+    $idMessage = $events['events'][0]['message']['id'];             
+}
+if(isset($events['events'][0]) && array_key_exists('postback',$events['events'][0])){
+    $is_postback = true;
+    $dataPostback = NULL;
+    parse_str($events['events'][0]['postback']['data'],$dataPostback);;
+    $paramPostback = NULL;
+    if(array_key_exists('params',$events['events'][0]['postback'])){
+        if(array_key_exists('date',$events['events'][0]['postback']['params'])){
+            $paramPostback = $events['events'][0]['postback']['params']['date'];
+        }
+        if(array_key_exists('time',$events['events'][0]['postback']['params'])){
+            $paramPostback = $events['events'][0]['postback']['params']['time'];
+        }
+        if(array_key_exists('datetime',$events['events'][0]['postback']['params'])){
+            $paramPostback = $events['events'][0]['postback']['params']['datetime'];
+        }                       
+    }
+}   
+if(!is_null($is_postback)){
+    $textReplyMessage = "ข้อความจาก Postback Event Data = ";
+    if(is_array($dataPostback)){
+        $textReplyMessage.= json_encode($dataPostback);
+    }
+    if(!is_null($paramPostback)){
+        $textReplyMessage.= " \r\nParams = ".$paramPostback;
+    }
+    $case = 1;
+    $userMessage = $textReplyMessage;     
+}
+
+
+///////////////////////////////////////////////////
             if($typeMessage=='text'){
                 if(!is_null($events)){
                     $userMessage = $events['events'][0]['message']['text'];
-                }
-
-                if(isset($events['events'][0]) && array_key_exists('postback',$events['events'][0])){
-                    $is_postback = true;
-                    $dataPostback = NULL;
-                    parse_str($events['events'][0]['postback']['data'],$dataPostback);;
-                    $paramPostback = NULL;
-                    if(array_key_exists('params',$events['events'][0]['postback'])){
-                        if(array_key_exists('date',$events['events'][0]['postback']['params'])){
-                            $paramPostback = $events['events'][0]['postback']['params']['date'];
-                        }
-                        if(array_key_exists('time',$events['events'][0]['postback']['params'])){
-                            $paramPostback = $events['events'][0]['postback']['params']['time'];
-                        }
-                        if(array_key_exists('datetime',$events['events'][0]['postback']['params'])){
-                            $paramPostback = $events['events'][0]['postback']['params']['datetime'];
-                        }                       
-                    }
-                }   
-                if(!is_null($is_postback)){
-                    $textReplyMessage = "ข้อความจาก Postback Event Data = ";
-                    if(is_array($dataPostback)){
-                        $textReplyMessage.= json_encode($dataPostback);
-                    }
-                    if(!is_null($paramPostback)){
-                        $textReplyMessage.= " \r\nParams = ".$paramPostback;
-                    }
-                    $textMessageBuilder = new TextMessageBuilder($textReplyMessage);     
                 }
 
             //ลงทะเบียน
