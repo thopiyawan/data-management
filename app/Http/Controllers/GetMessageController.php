80e318db6906f856bdc1109c998a25c0aa480832
piyawan <tho.piyawan@gmail.com>
2019-11-16 06:12:23 +0700

    Update GetMessageController.php

--- app/Http/Controllers/GetMessageController.php
+++ app/Http/Controllers/GetMessageController.php
@@ -411,7 +411,7 @@ if(!is_null($events)){
                     //ขอทราบวันออกเดินทางจากประเทศไทยค่ะ?
                         }elseif(strpos($userMessage, 'Params=') !== false ){
                             //is_string($userMessage) !== false &&  $seqcode == '007' ||
-                            $$userMessage = json_encode($userMessage);
+                            $userMessage = json_encode($userMessage);
                             $arr = explode("=", $userMessage, 2);
                             $userMessage= $arr[2];
 
