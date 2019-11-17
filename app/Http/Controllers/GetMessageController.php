34ebe21388ef4e38980239cd889512d7ad7f151c
piyawan <tho.piyawan@gmail.com>
2019-11-17 11:17:16 +0700

    Update GetMessageController.php

--- app/Http/Controllers/GetMessageController.php
+++ app/Http/Controllers/GetMessageController.php
@@ -155,7 +155,7 @@ class GetMessageController extends Controller
         //$register_update = pg_exec($dbconn, "UPDATE orders SET  dStart = '{$val}' WHERE userID = '{$user}' ") or die(pg_errormessage());  
         $startdate = $this->order_select($user);
     
-        dd($startdate);  
+        print($startdate);  
         // $re = pg_fetch_all($result);
         //  $result ;
         // $seqcode ='001';
@@ -749,7 +749,7 @@ if(!is_null($events)){
         $dbconn = pg_pconnect($conn_string);
         $result = pg_query($dbconn,"SELECT * FROM orders WHERE userid = '{$user} ORDER BY id DESC limit 1'");
                 while ($row = pg_fetch_object($result)) {
-                   return $row;
+                   return $row->dstart;
                 } 
     }
     public function country_select()
