3946d88ecc3924d63ed15d3b729804d36e5aa5c6
piyawan <tho.piyawan@gmail.com>
2019-11-22 06:48:21 +0700

    Create travellog.blade.php

---
+++ resources/views/travellog.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>บันทึกการทานวิตามิน</title>
+
+</head>
+<meta charset="utf-8">
+<link href="https://fonts.googleapis.com/css?family=Kanit" rel="stylesheet">
+<!-- <link rel="stylesheet" href="css/stylecss_pploy.css" /> -->
+<link rel="stylesheet" href="{{URL::asset('css/stylecss_pploy.css')}}">
+<link href="https://fonts.googleapis.com/css?family=Abril+Fatface" rel="stylesheet">
+
+<style type="text/css">
+ol {
+  list-style: none;
+  counter-reset: my-awesome-counter;
+/*  display: flex;
+  flex-wrap: wrap;*/
+  margin: 0;
+  padding: 0;
+  text-align: left;
+}
+
+  
+h2{
+  -webkit-margin-before: 0px;
+    -webkit-margin-after: 0px;
+}
+</style>
+
+      <div class="content card">
+       <h1>บันทึกการทานวิตามิน</h1>
+      <div>
+  <table >
+    <tbody>
+          <tr>
+              <td><h2></h2>
+                
+              </td>
+              
+            
+             <td>
+                <ol>
+                  <li><img class="food" src="{{URL::asset('img_web/lime.png')}}" /> </li>
+                </ol>
+                
+              </td>
+          
+          </tr>
+
+   </tbody>
+
+</table></div></div>
+<img class="foot" src="{{URL::asset('css/bg_forest.png')}}" />
+</body>
+</html>
+
+
+
