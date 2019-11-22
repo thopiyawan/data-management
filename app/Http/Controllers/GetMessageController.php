2b16602f80f654a84b82c877b41ddb28f5de8e7a
piyawan <tho.piyawan@gmail.com>
2019-11-22 11:08:20 +0700

    Update GetMessageController.php

--- app/Http/Controllers/GetMessageController.php
+++ app/Http/Controllers/GetMessageController.php
@@ -122,6 +122,8 @@ class GetMessageController extends Controller
 
         $conn_string = "host=ec2-50-19-127-115.compute-1.amazonaws.com port=5432 dbname=d7g7emtks53g61 user=unzugplrlxhlus password=6c4119aeed2e68f47cb7f66d964e9d984471a6fc2bdabadba149f298eb40aa6b";
         $dbconn = pg_pconnect($conn_string);
+
+
         // $user = 'U2dc636d2cd052e82c29f5284e00f69b9';
         // $fullname = 'ploy';
         // $result = pg_query($dbconn,"SELECT seqcode FROM sequentsteps WHERE sender_id = '5555'");
@@ -163,7 +165,63 @@ class GetMessageController extends Controller
         // $a = $this->order_select_10($user);
         $rows = $this->order_select_10_count($user);
       
-        print(  $rows->num_rows());
+         $order10s = $this->order_select_10;
+                   
+                        $columnTemplateBuilders = [];
+
+                        foreach ($order10s as $record) {
+        
+                      
+                            $columnTemplateBuilder =  
+                                [
+                                  // $record->created_at
+                                      'type' => 'box',
+                                      'layout' => 'horizontal',
+                                      'margin' => 'xxl',
+                                      'spacing' => 'sm',
+                                      'contents' => 
+                                      array (
+                                        0 => 
+                                        array (
+                                          'type' => 'text',
+                                          'text' => $record['id'],
+                                          'size' => 'sm',
+                                          'color' => '#555555',
+                                          'wrap' => true,
+                                        ),
+                                        1 => 
+                                        array (
+                                          'type' => 'text',
+                                          'text' => $record['id'],
+                                          'size' => 'md',
+                                          'color' => '#2E7D32',
+                                          'align' => 'center',
+                                          'wrap' => true,
+                                          'flex' => 2,
+                                        ),
+                                        2 => 
+                                        array (
+                                          'type' => 'button',
+                                          'style' => 'primary',
+                                          'color' => '#B2EBF2',
+                                          'height' => 'sm',
+                                          'flex' => 0,
+                                          'action' => 
+                                          array (
+                                           'type' => 'message',
+                                           'label' => '✏',
+                                           'text' => $record['id'],
+                                          ),
+                                        ),
+                                      ),
+                                    ]
+                                    ;
+                            array_push($columnTemplateBuilders, $columnTemplateBuilder);
+                        } 
+        
+                      $c = count($columnTemplateBuilders);
+
+                      dd($c);
         // foreach($a as $array)
         //     {
         //         $array['id'];
