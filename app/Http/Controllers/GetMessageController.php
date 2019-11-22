b7725b9873f2891ce327c25b994cf9c7b553d8ed
piyawan <tho.piyawan@gmail.com>
2019-11-22 16:03:42 +0700

    Update GetMessageController.php

--- app/Http/Controllers/GetMessageController.php
+++ app/Http/Controllers/GetMessageController.php
@@ -122,9 +122,12 @@ class GetMessageController extends Controller
 
         $conn_string = "host=ec2-50-19-127-115.compute-1.amazonaws.com port=5432 dbname=d7g7emtks53g61 user=unzugplrlxhlus password=6c4119aeed2e68f47cb7f66d964e9d984471a6fc2bdabadba149f298eb40aa6b";
         $dbconn = pg_pconnect($conn_string);
+        $user = 'U2dc636d2cd052e82c29f5284e00f69b9';
+        $int = (int)$val;
+        $register_update = pg_exec($dbconn, "UPDATE users SET  age = '{$val}' WHERE lineid = '{$user}' ") or die(pg_errormessage());  
 
 
-        // $user = 'U2dc636d2cd052e82c29f5284e00f69b9';
+        
         // $fullname = 'ploy';
         // $result = pg_query($dbconn,"SELECT seqcode FROM sequentsteps WHERE sender_id = '5555'");
         //$register_insert = pg_exec($dbconn, "INSERT INTO users(lineid,fullname,email,tel,dActive)VALUES('{$user}','{$fullname}','NULL','NULL','1')") or die(pg_errormessage());
@@ -1820,7 +1823,7 @@ if(!is_null($events)){
             case '002':
 
                     $int = (int)$val;
-                    $register_update = pg_exec($dbconn, "UPDATE users SET  age = '{$val}' WHERE lineid = '{$user}' ") or die(pg_errormessage());  
+                    $register_update = pg_exec($dbconn, "UPDATE users SET  age = '{$int}' WHERE lineid = '{$user}' ") or die(pg_errormessage());  
                     return $register_update;
                 break;
             case '003':
@@ -1875,21 +1878,4 @@ if(!is_null($events)){
 
         }
     }
-    public function hislog($user)
-    {
-
-           $user = 'U2dc636d2cd052e82c29f5284e00f69b9';
-           $conn_string = "host=ec2-50-19-127-115.compute-1.amazonaws.com port=5432 dbname=d7g7emtks53g61 user=unzugplrlxhlus password=6c4119aeed2e68f47cb7f66d964e9d984471a6fc2bdabadba149f298eb40aa6b";
-           $dbconn = pg_pconnect($conn_string);
-           $result = pg_query($dbconn,"SELECT * FROM orders WHERE userid = '{$user}' and dactive = 1 ORDER BY id DESC limit 10");
-           $re = pg_fetch_all($result);
-
-
-        return View::make('travellog')->with('record',$re);
-    }
-
-
-
-    
-
 }  
