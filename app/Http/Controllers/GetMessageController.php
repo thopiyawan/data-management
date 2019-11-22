be3c1f04e05114c81ee0e09ec2cfbd92fc1f7c72
piyawan <tho.piyawan@gmail.com>
2019-11-22 09:32:37 +0700

    Update GetMessageController.php

--- app/Http/Controllers/GetMessageController.php
+++ app/Http/Controllers/GetMessageController.php
@@ -161,7 +161,7 @@ class GetMessageController extends Controller
         // $datediff = $val - $startd1;
         // $val1 = round(($datediff / (60 * 60 * 24))+1);
         $a = $this->order_select_10($user);
-        print($a->totalprice);
+        print($a['id']);
         //dd($a);
         // $re = pg_fetch_all($result);
         //  $result ;
@@ -1248,10 +1248,8 @@ if(!is_null($events)){
         $conn_string = "host=ec2-50-19-127-115.compute-1.amazonaws.com port=5432 dbname=d7g7emtks53g61 user=unzugplrlxhlus password=6c4119aeed2e68f47cb7f66d964e9d984471a6fc2bdabadba149f298eb40aa6b";
         $dbconn = pg_pconnect($conn_string);
         $result = pg_query($dbconn,"SELECT * FROM orders WHERE userid = '{$user}' and dactive = 1 ORDER BY id DESC limit 10");
-        while ($row = pg_fetch_object($result)) {
-            return $row;
-         } 
-        return $row;
+        $re = pg_fetch_all($result);
+        return $re;
     }
     public function country_select()
     {
